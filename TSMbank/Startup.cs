dddaf79dfa99d8515992432d396ac6ac508bd10e
starman <th.sarigiorgis@gmail.com>
2019-06-22 02:20:27 +0300

    Add project files.

---
+++ TSMbank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TSMbank.Startup))]
+namespace TSMbank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
