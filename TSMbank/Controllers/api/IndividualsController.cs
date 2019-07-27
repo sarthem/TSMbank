4c64106971db7627f3f822eeb6bb23d40694cd9d
starman <th.sarigiorgis@gmail.com>
2019-07-27 12:05:23 +0300

    fixSavings

--- TSMbank/Controllers/api/IndividualsController.cs
+++ TSMbank/Controllers/api/IndividualsController.cs
@@ -23,10 +23,7 @@ namespace TSMbank.Controllers.api
             context.SaveChanges();
             return Ok();
         }
-
-
-
-
+                
         public IndividualsController()
         {
             context = new ApplicationDbContext();
